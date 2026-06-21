cf40a0d35fac9e1ee703e11b7abd448b8cea70f5
vdeepikaa24 <your_email@example.com>
2026-06-21 21:41:53 +0530

    JUnit_Advanced_Testing - Exercise - 2

---
+++ Java FSE/Deepskilling/Week-01/src/test/java/junit/JUnit_Advanced_Testing_Exercises/Exercise_2_Tests_Suites_And_Categories.java
@@ -0,0 +1,12 @@
+package junit.JUnit_Advanced_Testing_Exercises;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Exercise_2_Tests_Suites_And_Categories {
+
+    @Test
+    public void testExample() {
+        assertTrue(true);
+    }
+}
