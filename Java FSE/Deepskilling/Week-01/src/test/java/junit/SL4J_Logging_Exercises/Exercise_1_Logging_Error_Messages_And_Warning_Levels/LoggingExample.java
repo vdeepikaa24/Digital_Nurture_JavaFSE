45f48b0c38964d12dbcaf207b9a6954d6570caf8
vdeepikaa24 <your_email@example.com>
2026-06-24 13:00:00 +0530

    SL4J Logging - Exercise - 1

---
+++ Java FSE/Deepskilling/Week-01/src/test/java/junit/SL4J_Logging_Exercises/Exercise_1_Logging_Error_Messages_And_Warning_Levels/LoggingExample.java
@@ -0,0 +1,11 @@
+package junit.SL4J_Logging_Exercises.Exercise_1_Logging_Error_Messages_And_Warning_Levels;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class LoggingExample {
+ private static final Logger logger = LoggerFactory.getLogger(LoggingExample.class);
+ public static void main(String[] args) {
+ logger.error("This is an error message");
+ logger.warn("This is a warning message");
+ }
+}
