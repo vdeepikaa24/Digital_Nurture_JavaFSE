8c446a68c5ea56a9b515bce4a3cdfce842a4f055
vdeepikaa24 <your_email@example.com>
2026-06-28 21:24:35 +0530

    chore: remove redundant folders to prepare for clean com.library structure

--- Java FSE/Deepskilling/Week-02/src/main/java/springcore_maven/exercise_1_configuring_a_basic_spring_application/com/library/repository/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package springcore_maven.exercise_1_configuring_a_basic_spring_application.com.library.repository;
-public class BookRepository {
-    public void getInfo() {
-        System.out.println("Repository: Book data retrieved.");
-    }
-}
--- Java FSE/Deepskilling/Week-02/src/main/java/springcore_maven/exercise_2_implementing_dependency_injection/Repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package springcore_maven.exercise_2_implementing_dependency_injection.com.library.repository;
-
-public class BookRepository {
-    public void getInfo() {
-        System.out.println("Repository: Book data retrieved.");
-    }
-}
