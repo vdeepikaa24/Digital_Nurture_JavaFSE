c572ccd4d5455dd8cc0f68867edfe39943d2ebde
vdeepikaa24 <your_email@example.com>
2026-06-29 14:17:17 +0530

    feat: complete Exercise 9 and clean up git tracking

---
+++ Java FSE/Deepskilling/Week-02/LibraryManagement/LibraryManagement/src/main/java/com/library/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.library.controller;
+
+import com.library.entity.Book;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
---
+++ Java FSE/Deepskilling/Week-02/LibraryManagement/LibraryManagement/src/test/java/com/library/LibraryManagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.library;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
