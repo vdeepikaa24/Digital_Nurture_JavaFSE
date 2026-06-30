a3b7e11dabae8a6b65c39ac86a4b3d2192e914b8
vdeepikaa24 <your_email@example.com>
2026-06-30 22:32:39 +0530

    Completed Hands-on 6: Country Service and Exception Handling

--- Java FSE/Deepskilling/Week-02/Spring Data JPA/orm-learn/orm-learn-hands-on-6/orm-learn-hands-on-6/src/main/java/com/cognizant/orm_learn/exception/CountryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.orm_learn.exception;
-
-public class CountryNotFoundException extends Exception {
-    public CountryNotFoundException(String message) {
-        super(message);
-    }
-}
