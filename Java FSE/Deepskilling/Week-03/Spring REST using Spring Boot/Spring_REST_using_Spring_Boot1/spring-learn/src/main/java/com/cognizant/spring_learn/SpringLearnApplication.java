cdebbe1dc1000cbac3061871b59e137c1dfc5e53
vdeepikaa24 <your_email@example.com>
2026-07-03 15:22:23 +0530

    Hands-on-2: Spring Core – Load SimpleDateFormat from Spring Configuration XML

--- Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/Spring_REST_using_Spring_Boot1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/Spring_REST_using_Spring_Boot1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -2,12 +2,30 @@ package com.cognizant.spring_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
-	    System.out.println("Inside main");
-	    SpringApplication.run(SpringLearnApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Inside main");
+        SpringApplication.run(SpringLearnApplication.class, args);
+        
+        displayDate();
+    }
+
+public static void displayDate() {
+    try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml")) {
+        
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+
+        Date date = format.parse("31/12/2018");
+        System.out.println("Parsed Date: " + date);
+        
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 }
