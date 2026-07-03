a8a83ca1b0053eb4e66f7fc7d4dc322b4636bdb0
vdeepikaa24 <your_email@example.com>
2026-07-03 15:42:19 +0530

    Hands-on-6: Spring Core - Load list of countries from XML

--- Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/Spring_REST_using_Spring_Boot1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/Spring_REST_using_Spring_Boot1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Date; 
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -19,40 +20,42 @@ public class SpringLearnApplication {
         
         displayDate();
         displayCountry();
+        displayCountries();
         
         LOGGER.info("END");
     }
 
     public static void displayDate() {
         LOGGER.info("START");
-        
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml")) {
             SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
             Date date = format.parse("31/12/2018");
-            
             LOGGER.debug("Date: {}", date);
-            
         } catch (Exception e) {
             LOGGER.error("Error parsing date", e);
         }
-        
         LOGGER.info("END");
     }
 
-    public static void displayCountry() {
+   public static void displayCountry() {
     LOGGER.info("START");
     try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
-        Country country = context.getBean("country", Country.class);
-        Country anotherCountry = context.getBean("country", Country.class);
-        
+        Country country = context.getBean("in", Country.class); 
         LOGGER.debug("Country : {}", country.toString());
-        LOGGER.debug("Another Country : {}", anotherCountry.toString());
-        
-        LOGGER.debug("Are they the same instance? {}", (country == anotherCountry));
-        
     } catch (Exception e) {
         LOGGER.error("Error retrieving country bean", e);
     }
     LOGGER.info("END");
 }
+    @SuppressWarnings("unchecked") 
+    public static void displayCountries() {
+        LOGGER.info("START");
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
+            ArrayList<Country> countries = (ArrayList<Country>) context.getBean("countryList");
+            LOGGER.debug("Countries : {}", countries);
+        } catch (Exception e) {
+            LOGGER.error("Error retrieving country list", e);
+        }
+        LOGGER.info("END");
+    }
 }
