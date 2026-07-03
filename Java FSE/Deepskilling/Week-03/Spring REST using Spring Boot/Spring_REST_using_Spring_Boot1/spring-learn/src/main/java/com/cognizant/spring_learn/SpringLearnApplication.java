255a43df8c7c3d99184a2da697b533a520537d4a
vdeepikaa24 <your_email@example.com>
2026-07-03 15:28:12 +0530

    Hands-on-3: Spring Core - Incorporate Logging

--- Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/Spring_REST_using_Spring_Boot1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/Spring_REST_using_Spring_Boot1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,5 +1,7 @@
 package com.cognizant.spring_learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,23 +11,28 @@ import java.util.Date;
 @SpringBootApplication
 public class SpringLearnApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("Inside main");
+        LOGGER.info("START");
         SpringApplication.run(SpringLearnApplication.class, args);
-        
         displayDate();
+        LOGGER.info("END");
     }
 
-public static void displayDate() {
-    try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml")) {
+    public static void displayDate() {
+        LOGGER.info("START");
         
-        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-
-        Date date = format.parse("31/12/2018");
-        System.out.println("Parsed Date: " + date);
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml")) {
+            SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+            Date date = format.parse("31/12/2018");
+            
+            LOGGER.debug("Date: {}", date);
+            
+        } catch (Exception e) {
+            LOGGER.error("Error parsing date", e);
+        }
         
-    } catch (Exception e) {
-        e.printStackTrace();
+        LOGGER.info("END");
     }
-}
 }
