abb42813c7d1aad918db4bc099c9e7b6fa20e2e1
vdeepikaa24 <your_email@example.com>
2026-07-07 13:53:48 +0530

    Validating country code

--- Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/FullStackApp/Spring_REST_using_Spring_Boot4/spring-rest-service/src/main/java/com/cognizant/spring_rest_service/controller/CountryController.java
+++ Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/FullStackApp/Spring_REST_using_Spring_Boot4/spring-rest-service/src/main/java/com/cognizant/spring_rest_service/controller/CountryController.java
@@ -2,41 +2,46 @@ package com.cognizant.spring_rest_service.controller;
 
 import org.springframework.web.bind.annotation.*;
 import com.cognizant.spring_rest_service.model.Country;
-import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import jakarta.validation.ConstraintViolation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.List;
 
 @RestController
-@RequestMapping("/countries") 
+@RequestMapping("/countries")
 public class CountryController {
 
-    // 1. GET All
-    @GetMapping
-    public List<Country> getAllCountries() {
-        return null; 
-    }
-
-    // 2. GET Specific
-    @GetMapping("/{code}")
-    public Country getCountry(@PathVariable String code) {
-        return null;
-    }
-
-    // 3. POST (Create) - Updated to read Country bean from payload
     @PostMapping
-    public Country addCountry(@RequestBody @Valid Country country) {
+    public Country addCountry(@RequestBody Country country) {
         System.out.println("Start");
-        System.out.println(country.toString()); 
-        return country; 
-    }
+        System.out.println(country.toString());
 
-    // 4. PUT (Update)
-    @PutMapping
-    public Country updateCountry(@RequestBody @Valid Country country) {
+        // Create validator factory
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        // Validation is done against the annotations defined in country bean
+        Set<ConstraintViolation<Country>> violations = validator.validate(country);
+        List<String> errors = new ArrayList<String>();
+
+        // Accumulate all errors in an ArrayList of type String
+        for (ConstraintViolation<Country> violation : violations) {
+            errors.add(violation.getMessage());
+        }
+
+        // Throw exception so that the user receives an appropriate error message
+        if (violations.size() > 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors.toString());
+        }
+
         return country;
-    }
-
-    // 5. DELETE
-    @DeleteMapping("/{code}")
-    public void deleteCountry(@PathVariable String code) {
     }
 }
--- Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/FullStackApp/Spring_REST_using_Spring_Boot4/spring-rest-service/src/main/java/com/cognizant/spring_rest_service/model/Country.java
+++ Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/FullStackApp/Spring_REST_using_Spring_Boot4/spring-rest-service/src/main/java/com/cognizant/spring_rest_service/model/Country.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Country {
-    
+
     @NotNull
-    @Size(min=2, max=2, message="Country code should be 2 characters")
+    @Size(min = 2, max = 2, message = "Country code should be 2 characters")
     private String code;
     
     private String name;
