249c2a68a72e1f1476c0b118750013a78bfd336e
vdeepikaa24 <your_email@example.com>
2026-07-07 14:00:03 +0530

    Include global exception handler for validation errors

--- Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/FullStackApp/Spring_REST_using_Spring_Boot4/spring-rest-service/src/main/java/com/cognizant/spring_rest_service/controller/CountryController.java
+++ Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/FullStackApp/Spring_REST_using_Spring_Boot4/spring-rest-service/src/main/java/com/cognizant/spring_rest_service/controller/CountryController.java
@@ -2,46 +2,17 @@ package com.cognizant.spring_rest_service.controller;
 
 import org.springframework.web.bind.annotation.*;
 import com.cognizant.spring_rest_service.model.Country;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
-import jakarta.validation.ConstraintViolation;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/countries")
 public class CountryController {
 
     @PostMapping
-    public Country addCountry(@RequestBody Country country) {
+    public Country addCountry(@RequestBody @Valid Country country) {
         System.out.println("Start");
         System.out.println(country.toString());
-
-        // Create validator factory
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-
-        // Validation is done against the annotations defined in country bean
-        Set<ConstraintViolation<Country>> violations = validator.validate(country);
-        List<String> errors = new ArrayList<String>();
-
-        // Accumulate all errors in an ArrayList of type String
-        for (ConstraintViolation<Country> violation : violations) {
-            errors.add(violation.getMessage());
-        }
-
-        // Throw exception so that the user receives an appropriate error message
-        if (violations.size() > 0) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errors.toString());
-        }
-
+        
         return country;
     }
 }
---
+++ Java FSE/Deepskilling/Week-03/Spring REST using Spring Boot/FullStackApp/Spring_REST_using_Spring_Boot4/spring-rest-service/src/main/java/com/cognizant/spring_rest_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.cognizant.spring_rest_service.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        LOGGER.info("Start");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+        
+        LOGGER.info("End");
+        return new ResponseEntity<>(body, headers, status);
+    }
+}
