2a724d507c53ba0fce6e22b90fb333f1b03a2322
vdeepikaa24 <your_email@example.com>
2026-07-11 16:14:27 +0530

    Exercise 3: Resilience Patterns in an API Gateway

---
+++ Java FSE/Deepskilling/Week-04/Microservices with Spring Boot 3 and Spring Cloud/Exercise_3/resiliencegateway/src/main/java/com/cognizant/resiliencegateway/ResiliencegatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.resiliencegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiliencegatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiliencegatewayApplication.class, args);
+	}
+
+}
---
+++ Java FSE/Deepskilling/Week-04/Microservices with Spring Boot 3 and Spring Cloud/Exercise_3/resiliencegateway/src/test/java/com/cognizant/resiliencegateway/ResiliencegatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.resiliencegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiliencegatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
