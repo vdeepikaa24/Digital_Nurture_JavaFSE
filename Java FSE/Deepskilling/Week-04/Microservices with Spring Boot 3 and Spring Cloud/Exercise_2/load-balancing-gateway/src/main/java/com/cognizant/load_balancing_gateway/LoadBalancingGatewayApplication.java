518fa2fbea3c11928372952908638c400a870bcb
vdeepikaa24 <your_email@example.com>
2026-07-11 15:46:40 +0530

    Exercise 2: Load Balancing in an API Gateway

---
+++ Java FSE/Deepskilling/Week-04/Microservices with Spring Boot 3 and Spring Cloud/Exercise_2/load-balancing-gateway/src/main/java/com/cognizant/load_balancing_gateway/LoadBalancingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.load_balancing_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancingGatewayApplication.class, args);
+    }
+
+}
---
+++ Java FSE/Deepskilling/Week-04/Microservices with Spring Boot 3 and Spring Cloud/Exercise_2/load-balancing-gateway/src/test/java/com/cognizant/load_balancing_gateway/LoadBalancingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.load_balancing_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
