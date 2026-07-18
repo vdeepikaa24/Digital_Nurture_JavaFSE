4f6178216da6b8840745b37d5655a7cef73b527e
vdeepikaa24 <your_email@example.com>
2026-07-18 10:28:28 +0530

    feat: implement OnlineShopping lab with prop-based data passing

---
+++ Java FSE/Deepskilling/Week-05/7.ReactJS-HOL/shoppingapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OnlineShopping from './Component/OnlineShopping'; 
+
+function App() {
+  return (
+    <div className="App">
+      <OnlineShopping />
+    </div>
+  );
+}
+
+export default App;
